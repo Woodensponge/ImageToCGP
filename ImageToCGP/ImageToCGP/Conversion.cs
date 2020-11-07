9440314fb2b136934b0775258e705b55b966624e
Woodensponge <43834352+Woodensponge@users.noreply.github.com>
2020-11-06 20:39:39 -0500

    Update Conversion.cs

--- ImageToCGP/ImageToCGP/Conversion.cs
+++ ImageToCGP/ImageToCGP/Conversion.cs
@@ -9,6 +9,11 @@ namespace ImageToCGP
         {
             Bitmap image = (Bitmap)Image.FromFile(file);
             image.SetResolution(16f, 16f);
+            for (int x = 0; x <= image.Width; x++)
+                for (int y = 0; y <= image.Height; y++)
+                {
+
+                }
             return 0;
         }
     }
